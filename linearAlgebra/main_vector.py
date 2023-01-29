9ba935a52fe5fdf9ad1d1aa17108307bd94b6455
mawenbo <631714470@qq.com>
2023-01-29 18:03:24 +0800

    单位向量

--- linearAlgebra/main_vector.py
+++ linearAlgebra/main_vector.py
@@ -6,4 +6,19 @@ if __name__ == "__main__":
     print(vec)
     print(len(vec))
     print("vec[0] = {}, vec[1] = {}".format(vec[0],vec[1]))
-    vec.__delattr__()
+
+    vec2 = Vector([3, 1])
+    print("{} + {} = {}".format(vec, vec2, vec + vec2))
+    print("{} * {} = {}".format(vec, 3, vec * 3))
+    print("+{} = {}".format(vec, +vec))
+    print("-{} = {}".format(vec, -vec))
+
+    zero2 = Vector.zero(2)
+    print(zero2)
+    print("{} + {} = {}".format(vec,zero2,vec + zero2))
+
+    print("norm({}) = {}".format(vec,vec.norm()))
+
+    print("normalize {} is {}".format(vec,vec.normalize()))
+
+    print(vec.normalize().norm())
--- linearAlgebra/playLa/Vector.py
+++ linearAlgebra/playLa/Vector.py
@@ -1,12 +1,47 @@
+import math
+
+
 class Vector:
     def __init__(self, lst):
         # 对入参做一个复制，防止对入参修改影响 values
         self._values = list(lst)
 
-    def __add__(self, another):
+    @classmethod
+    def zero(cls, dim):
+        """返回一个dim维的零向量"""
+        return cls([0] * dim)
+
+    def norm(self):
+        """返回向量的模"""
+        return math.sqrt(sum(e**2 for e in self))
+
+    def normalize(self):
+         return  1 / self.norm() * Vector(self._values)
+
+    def __add__(self, other):
         """向量加法，返回结果向量"""
-        assert len(self) == len(another), "Error in adding. Length of vectors must be same"
-        return Vector([a + b for a, b in zip(self, another)])
+        assert len(self) == len(other), "Error in adding. Length of vectors must be same"
+        return Vector([a + b for a, b in zip(self, other)])
+
+    def __sub__(self, other):
+        """向量减法"""
+        assert len(self) == len(other), "Error in subtracting. Length of vectors must be same"
+        return Vector([a - b for a,b in zip(self, other)])
+
+    def __mul__(self, other):
+        """向量乘法（右乘） self * other"""
+        return Vector([ a * other for a in self])
+
+    def __rmul__(self, other):
+        """向量乘法（左乘） other * self"""
+        return self * other
+
+    def __pos__(self):
+        """向量取正"""
+        return 1 * self
+    def __neg__(self):
+        """向量取负"""
+        return -1 * self
     def __iter__(self):
         """返回向量的迭代器"""
         return self._values.__iter__()
@@ -25,3 +60,5 @@ class Vector:
     def __str__(self):
         # 重写str方法。用户主动调用的输出对象的方法
         return "({})".format(", ".join(str(e) for e in self._values))
+
+
